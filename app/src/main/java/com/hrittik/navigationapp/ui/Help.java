389d194136bc0020f5484f74d7f8f527ae7d7fde
Hrittik <hrittikgarain@gmail.com>
2021-06-09 12:57:44 +0530

    This is my first commit

---
+++ app/src/main/java/com/hrittik/navigationapp/ui/Help.java
@@ -0,0 +1,38 @@
+package com.hrittik.navigationapp.ui;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hrittik.navigationapp.R;
+
+public class Help extends Fragment {
+
+    private HelpViewModel mViewModel;
+
+    public static Help newInstance() {
+        return new Help();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.help_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(HelpViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/hrittik/navigationapp/ui/HelpViewModel.java
@@ -0,0 +1,7 @@
+package com.hrittik.navigationapp.ui;
+
+import androidx.lifecycle.ViewModel;
+
+public class HelpViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
